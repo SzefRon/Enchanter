ef41b1366b1abce14a2d7b82e3545bbbca423e75
SzefRon <ziome550@wp.pl>
2024-11-15 17:49:55 +0100

    functioning fft with windowing

--- src/PluginProcessor.cpp
+++ src/PluginProcessor.cpp
@@ -89,6 +89,10 @@ void AudioPluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPer
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
     juce::ignoreUnused (sampleRate, samplesPerBlock);
+    setLatencySamples(fft[0].getLatencyInSamples());
+
+    fft[0].reset();
+    fft[1].reset();
 }
 
 void AudioPluginAudioProcessor::releaseResources()
@@ -124,32 +128,30 @@ bool AudioPluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layou
 void AudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
                                               juce::MidiBuffer& midiMessages)
 {
-    juce::ignoreUnused (midiMessages);
-
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
+    auto numInputChannels = getTotalNumInputChannels();
+    auto numOutputChannels = getTotalNumOutputChannels();
+    auto numSamples = buffer.getNumSamples();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+    for (auto i = numInputChannels; i < numOutputChannels; ++i) {
+        buffer.clear(i, 0, numSamples);
+    }
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-        juce::ignoreUnused (channelData);
-        // ..do something to the data...
+    bool bypassed = false;
+
+    float* channelL = buffer.getWritePointer(0);
+    float* channelR = buffer.getWritePointer(1);
+
+    // Processing on a sample-by-sample basis:
+    for (int sample = 0; sample < numSamples; ++sample) {
+        float sampleL = channelL[sample];
+        float sampleR = channelR[sample];
+
+        sampleL = fft[0].processSample(sampleL, bypassed);
+        sampleR = fft[1].processSample(sampleR, bypassed);
+
+        channelL[sample] = sampleL;
+        channelR[sample] = sampleR;
     }
 }
 
